1babfdfe0f76ba1b4112540e7ceaddfea200925a
David Boclé <david.bocle@finastra.com>
2024-04-10 11:21:11 +0200

    google analytics via service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), icon()],
+  site: "https://www.twoway.finance/",
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [
+    sitemap(),
+    tailwind(),
+    icon(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ], // Add renderers to the config
+  // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
